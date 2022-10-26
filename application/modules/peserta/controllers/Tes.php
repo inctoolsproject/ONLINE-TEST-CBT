a5fc018eb4f873d457c02d292849b639c5f19a32
Bastian Nazaromi <bastian.nazaromi@gmail.com>
2022-10-26 16:57:59 +0700

    eea

--- application/modules/peserta/controllers/Tes.php
+++ application/modules/peserta/controllers/Tes.php
@@ -10,8 +10,10 @@ class Tes extends MX_Controller
 		peserta_init();
 	}
 
-	public function index($no = null)
+	public function index()
 	{
+		$today = date('Y-m-d H:i:s');
+
 		$id_list_ujian = $this->session->userdata('id_list_ujian');
 
 		$list_ujian = $this->peserta->getOneListUjian(['list_ujian.id' => $id_list_ujian]);
@@ -25,6 +27,20 @@ class Tes extends MX_Controller
 		}
 
 		if ($timer) {
+			if ($today > $timer->selesai) {
+				$this->notifikasi->warning('Waktu ujian anda sudah berakhir');
+
+				redirect($_SERVER['HTTP_REFERER'], 'refresh');
+			}
+
+			$dataSoal = $this->universal->getMulti([
+				'id_list_ujian' => $id_list_ujian,
+				'id_user'       => $this->user->id
+			], 'jawaban_soal');
+
+			if (!$dataSoal) {
+				$this->_insertSoal($id_list_ujian, $list_ujian);
+			}
 		} else {
 			$dates = date('Y-m-d H:i:s');
 			$date = strtotime($dates);
@@ -38,6 +54,8 @@ class Tes extends MX_Controller
 			];
 
 			$this->universal->insert($data, 'timer');
+
+			$this->_insertSoal($id_list_ujian, $list_ujian);
 		}
 
 		$timer = $this->universal->getOne([
@@ -45,13 +63,121 @@ class Tes extends MX_Controller
 			'id_jenis_ujian' => $list_ujian->id_jenis_ujian
 		], 'timer');
 
-		echo json_encode($timer);
-		die;
+		$urut = dekrip($this->u3);
+
+		if ($urut) {
+			$urutan_soal = $this->universal->getOrderBy([
+				'id_list_ujian' => $id_list_ujian,
+				'id_user'       => $this->user->id,
+				'urutan'        => $urut
+			], 'jawaban_soal', 'urutan', 'ASC', '1');
+		} else {
+			$urutan_soal = $this->universal->getOrderBy([
+				'id_list_ujian' => $id_list_ujian,
+				'id_user'       => $this->user->id,
+				'jawaban'       => null
+			], 'jawaban_soal', 'urutan', 'ASC', '1');
+
+			if (!$urutan_soal) {
+				$urutan_soal = $this->universal->getOrderBy([
+					'id_list_ujian' => $id_list_ujian,
+					'id_user'       => $this->user->id
+				], 'jawaban_soal', 'urutan', 'DESC', '1');
+			}
+		}
+
+		$soal = $this->universal->getMulti(['id' => $urutan_soal[0]->id_soal], 'bank_soal');
+
+		$jawaban = [];
+		foreach ($soal as $hasil) {
+			$jwb = [
+				[
+					'pil' => 'a',
+					'jwb' => $hasil->a
+				],
+				[
+					'pil' => 'b',
+					'jwb' => $hasil->b
+				],
+				[
+					'pil' => 'c',
+					'jwb' => $hasil->c
+				],
+				[
+					'pil' => 'd',
+					'jwb' => $hasil->d
+				],
+				[
+					'pil' => 'e',
+					'jwb' => $hasil->e
+				],
+			];
+
+			shuffle($jwb);
+			$jwb = array_slice($jwb, 0, 5);
+
+			array_push($jawaban, [
+				$hasil->id => $jwb
+			]);
+		}
+
+		$allSoal = $this->universal->getMultiSelect('id, status, urutan', [
+			'id_list_ujian' => $id_list_ujian,
+			'id_user'       => $this->user->id
+		], 'jawaban_soal');
+
+		$allUrutan = [];
+		foreach ($urutan_soal as $aS) {
+			array_push($allUrutan, [
+				$aS->id_soal     => $aS
+			]);
+		}
+
 		$params = array(
-			'title' => 'Tes'
+			'title'         => 'Tes',
+			'waktu_selesai' => $timer->waktu_selesai,
+			'urutan'        => $urutan_soal,
+			'soal'          => $soal,
+			'allSoal'       => $allSoal,
+			'allUrutan'     => $allUrutan,
+			'jawaban'       => $jawaban
 		);
 
 		$this->load->view('tes', $params);
+	}
+
+	private function _insertSoal($id_list_ujian, $list_ujian)
+	{
+		$this->universal->delete([
+			'id_list_ujian' => $id_list_ujian,
+			'id_user'       => $this->user->id
+		], 'jawaban_soal');
+
+		$jumlahSoal = $list_ujian->jumlah_soal;
+
+		$data_soal = [];
+		$i = 1;
+
+		$soal = $this->universal->getOrderBy([
+			'id_jenis_ujian' => $list_ujian->id_jenis_ujian
+		], 'bank_soal', 'id', 'RANDOM', $jumlahSoal);
+
+		foreach ($soal as $sl) {
+			array_push($data_soal, [
+				'id_list_ujian' => $id_list_ujian,
+				'id_user'       => $this->user->id,
+				'id_soal'       => $sl->id,
+				'urutan'        => $i++,
+				'jawaban'       => null,
+				'status'        => 0
+			]);
+		}
+
+		$insert_soal = $this->universal->insert_batch($data_soal, 'jawaban_soal');
+
+		if (!$insert_soal) {
+			$this->notifikasi->error('Server sibuk, silahkan coba lagi');
+		}
 	}
 }
 
--- application/modules/peserta/views/tes.php
+++ application/modules/peserta/views/tes.php
@@ -9,17 +9,111 @@
 <?php $one->get_css('toastr/toastr.min.css'); ?>
 <?php $one->get_css('js/plugins/sweetalert2/sweetalert2.min.css'); ?>
 
+<style>
+	table {
+		border: none !important;
+		border-spacing: 0;
+		border-collapse: collapse;
+	}
+
+	p {
+		margin: auto !important;
+	}
+
+	.dropdown-menu {
+		max-height: 280px;
+		overflow-y: auto;
+	}
+</style>
+
 <?php require APPPATH . 'views/inc/_global/views/page_start.php'; ?>
 <?php include APPPATH . 'views/inc/peserta/views/inc_navigation.php'; ?>
 <div class="content">
 	<div class="row">
+		<div class="col-sm-12 text-right">
+			<div class="btn-group mb-3">
+				<div class="dropdown">
+					<button type="button" class="btn btn-secondary dropdown-toggle" id="dropdown-align-primary" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Daftar Soal
+					</button>
+					<div class="dropdown-menu dropdown-menu-right font-size-sm" aria-labelledby="dropdown-align-primary" style="width: 370px;">
+						<?php foreach ($allSoal as $hasil) : ?>
+							<?php if ($hasil->ragu == 0) {
+								$class_btn = 'btn-danger';
+							} else if ($hasil->ragu == 1) {
+								$class_btn = 'btn-success';
+							} else if ($hasil->ragu == 2) {
+								$class_btn = 'btn-warning';
+							}
+
+							?>
+							<button type="button" class="btn <?= $class_btn; ?> mb-2 ml-1" style="width: 63px;" id="<?= 'btn_soal' . $hasil->urutan; ?>" onclick="window.location='<?= base_url('peserta/tes/') . enkrip($hasil->urutan); ?>'"><?= $hasil->urutan; ?></button>
+						<?php endforeach; ?>
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="col-md-12">
 			<div class="block block-rounded">
-				<div class="block-header block-header-default">
-					<h3 class="block-title"></h3>
+				<?php
+				if ($urutan[0]->status == 0) {
+					$block_header = 'bg-danger';
+				} elseif ($urutan[0]->status == 1) {
+					$block_header = 'bg-warning';
+				} else {
+					$block_header = 'bg-success';
+				}
+				?>
+				<div class="block-header <?= $block_header; ?>">
+					<h3 class="block-title">No. <?= $urutan[0]->urutan; ?></h3>
 				</div>
 				<div class="block-content">
+					<?php foreach ($soal as $i => $sl) : ?>
+						<div class="row mb-1">
+							<div class="col-md-12">
+								<p class="h4 text-justify">
+									<?= $sl->soal; ?>
+								</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-12">
+								<table id="tabel_jawaban">
+									<?php foreach ($jawaban[$i][$sl->id] as $hasil) : ?>
+										<?php if ($hasil['jwb'] != null) : ?>
+											<tr>
+												<td width="40" height="50" class="text-center" valign="middle">
+													<input type="radio" class="radio_jwb" name="jawaban" value="<?= enkrip($hasil['pil']); ?>" <?= enkrip($hasil['pil']) == enkrip($allUrutan[$i][$sl->id]->jawaban) ? 'checked' : ''; ?>>
+												</td>
+												<td valign="middle" height="50">
+													<label class="label_jwb" data-jwb="<?= enkrip($hasil['pil']); ?>">
+														<p><?= $hasil['jwb']; ?></p>
+													</label>
+												</td>
+											</tr>
+										<?php endif; ?>
+									<?php endforeach; ?>
+								</table>
+							</div>
+						</div>
+					<?php endforeach; ?>
 
+					<div class="row mt-5 mb-3">
+						<div class="col-sm-12 text-right">
+							<?php if ($urutan[0]->urutan != 1) : ?>
+								<a href="<?= base_url('peserta/tes/') . enkrip($urutan[0]->urutan - 1); ?>" class="btn btn-danger prevEssay"><i class="fa fa-arrow-left"></i>&nbsp;Previous</a>
+							<?php endif; ?>
+
+							<a href="javascript:void(0)" id="ragu" data-id_jawaban="<?= enkrip($urutan[0]->id); ?>" data-urut="<?= $urutan[0]->urutan; ?>" class="btn btn-warning">&nbsp;Ragu-ragu</a>
+
+							<?php if ($urutan[0]->urutan != count($allSoal)) : ?>
+								<a href="<?= base_url('peserta/tes/') . enkrip($urutan[0]->urutan + 1); ?>" class="btn btn-primary nextEssay">Next&nbsp;<i class="fa fa-arrow-right"></i></a>
+							<?php endif; ?>
+							<?php if ($urutan[0]->urutan == count($allSoal)) : ?>
+								<a href="#" class="btn btn-success" data-target="#modal-akhiri" data-toggle="modal">Akhiri</a>
+							<?php endif; ?>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
